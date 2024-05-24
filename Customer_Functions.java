70ee4377fc297809eaff92fef122d6850c4292e2
TamkeenSara <163332245+TamkeenSara@users.noreply.github.com>
2024-05-24 19:01:41 +0500

    Delete Customer_Functions.java

--- Customer_Functions.java
+++
@@ -1,7 +0,0 @@
-public interface Customer_Functions {
-    void create_Booking(Booking booking);
-    void edit_Booking(Booking booking);
-    void cancel_Booking(Booking booking);
-    void display_all_bookings(int Customer_id);
-
-}
